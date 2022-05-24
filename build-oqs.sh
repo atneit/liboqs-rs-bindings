136fa7148d3c1af92ec90115bba339cff0ba9073
Alexander Nilsson <a.code@nilsson.link>
2022-05-24 20:38:40 +0200

    Updated message about dependencies

--- build-oqs.sh
+++ build-oqs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo If running debian/ubuntu install dependecies with the following command:
-echo sudo apt install cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz
+echo sudo apt install clang llvm gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz
 read -p "Press enter to continue"
 
 cd liboqs
